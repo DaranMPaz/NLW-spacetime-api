33c88b8b042a99313aaab3053524ba47d6da0749
Darlan Paz <rogue.one.dpaz@outlook.com>
2023-05-18 18:05:41 -0300

    Configuração da autenticação

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -58,9 +58,19 @@ export async function authRoutes(app: FastifyInstance) {
       })
     }
 
+    const token = app.jwt.sign(
+      {
+        name: user.name,
+        avatarUrl: user.avatarUrl,
+      },
+      {
+        sub: user.id,
+        expiresIn: '30 days',
+      },
+    )
+
     return {
-      userInfo,
-      user,
+      token,
     }
   })
 }
